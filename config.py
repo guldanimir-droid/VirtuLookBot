3121cbddcbd0945a4168578f34528d25657ffbc6
guldanimir-droid <guldanimir@gmail.com>
2026-04-08 14:43:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
 import os
 
-# --- секреты (берутся из переменных окружения) ---
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', '')
-FASHN_API_KEY  = os.getenv('FASHN_API_KEY', '')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+FASHN_API_KEY = os.getenv('FASHN_API_KEY')
+VIRTUAL_TRYON_SECRET = os.getenv('VIRTUAL_TRYON_SECRET', 'default-secret-change-me')
 WEBHOOK_URL_BASE = os.getenv('WEBHOOK_URL_BASE', '')
-
-# --- сеть ---
-# Для локального теста можно использовать ngrok, в продакшене Railway сам задаст WEBHOOK_URL_BASE
 WEBHOOK_URL_PATH = f"/bot/{TELEGRAM_TOKEN}" if TELEGRAM_TOKEN else "/bot/"
-
-# Порт для локального запуска (на Railway используется порт из переменной PORT)
 PORT = int(os.getenv('PORT', 8080))
